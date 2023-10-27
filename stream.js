8ebb8c2bb9d762de9b4b559ab79a186c893834a6
Peter Chan <peter1989212@gmail.com>
2023-10-28 01:55:40 +0800

    Update stream.js

--- stream.js
+++ stream.js
@@ -23,7 +23,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
       title: '流媒体解锁检测',
       content: '',
       icon: 'play.tv.fill',
-      'icon-color': '#922B21',
+      'icon-color': '#E74C3C',
     }
   let [{ region, status }] = await Promise.all([testDisneyPlus()])
     await Promise.all([check_youtube_premium(),check_netflix()])
